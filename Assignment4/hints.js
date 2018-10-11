04574d0e7bdccf4146de5e292ffaf7ddd995088c
BramBeer <bram.berendsen@outlook.com>
2018-10-11 16:35:58 +0200

    Finished assignment 4

--- Assignment4/hints.js
+++ Assignment4/hints.js
@@ -2,10 +2,13 @@
 dict = {};
 dict[41] = "The domain is defined by coordinates of two dimensions. Therefore, the domain is now defined by a 2 dimensional matrix. Type '?matrix()' in your console for more information.";
 dict[42] = "Remember that a Neumann natural boundary condition is assigned to unspecified boundaries.";
-dict[45] = "Note that you can surpress graphing of the node generation by setting 'verbose=False'.";
-dict[413] = "Increase both the domain values of 'xtopl' and 'ytopl' to interpolate for our two dimensional model domain.";
-dict[417] = "add: welldist = func.dist.to.point(c(1000,1000),200,25,100) and set.discretisation(TwoDModelWell, nodes=list(type="geomgen", optdist=welldist, verbose=TRUE), method="FE") on separate lines to decrease the nodal distance around the location of the well.";
-
+dict[45] = "Note that you can suppress graphing of the node generation by setting 'verbose=False'.";
+dict[413] = "Change both the domain values of 'xtopl' and 'ytopl' before interpolating the two dimensional model domain of TwoDmodel with the new transmissivities.";
+dict[417] = "add: welldist = func.dist.to.point(c(1000,1000),200,25,100) and set.discretisation(TwoDModelWell, nodes=list(type='geomgen', optdist=welldist, verbose=TRUE), method='FE') on separate lines to decrease the nodal distance around the location of the well.";
+dict[419] = "Check the documentation of the func.dist.to.line() function in the FVFE2D package.";
+dict[420] = "Define domain range: plot(c(0,10), c(0,2), xlab='x', ylab='y', col='white') Plot polygon: polygon(domain, lwd=3, col=rgb(0.7,0.7,0.7,0.3)).";
+dict[422] = "Remember the function plot.BC.id().";
+dict[425] = "The pressure function has 3 arguments: x, y and s. Its body contains one line: a return statement.";
 
 // returns the hint for the corresponding question_number in the corresponding paragraph field in the Rmd file.
 function showHint(question_number) {
