c6c44d3d03cb5641b50ae6a9969b11c1a613d636
BramBeer <bram.berendsen@outlook.com>
2018-11-13 17:48:27 +0100

    reviewed assignment1

--- hints.js
+++ hints.js
@@ -1,7 +1,7 @@
 // The hints are written in a dictionary, the key is the question number
 dict = {};
-dict[1] = "You can use the 'nodes' vector to calculate the analytical solution for all x's at the same time. Use substraction to look at the difference with the modelled heads. You can use the *dataframe.states()* function to get the modelled heads.";
-dict[2] = "(To be added: Explanation on Monin-Obukhov length/atmospheric stratification")
+dict[1] = "You can use the nodes-vector to calculate the analytical solution for all x positions at the same time. Use substraction to look at the difference with the modelled heads. You can use the *dataframe.states()* function to get the modelled heads.";
+dict[2] = "(To be added: Explanation on Monin-Obukhov length/atmospheric stratification)";
 
 // returns the hint for the corresponding question_number in the corresponding paragraph field in the Rmd file.
 function showHint(question_number) {
