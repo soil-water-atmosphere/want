13fdd9049b4c87301bcb87c7fdde27e2ab52aa60
BramBeer <bram.berendsen@outlook.com>
2018-11-16 17:04:53 +0100

    reviewed assignment3 meteo part1

--- Assignment2/hints.js
+++ Assignment2/hints.js
@@ -1,11 +1,12 @@
 // The hints are written in a dictionary, the key is the question number
 dict = {};
-dict[211] = "The model resembles the model made in the previous assignment.";
-dict[212] = "Check the arguments of the plot function of the FVFE1D package to make both plots in one line of code.";
-dict[213] = "Check the documentation of dataframe.balance().";
-dict[215] = "Read the documentation of the copy.model() function.";
-dict[2113] = "Check the arguments of the plot function of the FVFE1D package to make both plots in one line of code.";
-dict[2119] = "Before the loop construct is entered, you can basically copy the previous code that describes the two time increments. Do not forget to assign the proper state values to the oldstate function before entering the while loop.";
+dict[212] = "The model resembles the model made in the previous assignment.";
+dict[213] = "Check the documentation of the dataframe.* functions.";
+dict[214] = "Check the documentation of dataframe.balance().";
+dict[216] = "Read the documentation of the copy.model() function.";
+dict[218] = "It might seem a bit odd to use the function oldstate(x) here since it is not defined yet. However, as long as we only define the storageflux function and not actually run it, we are allowed to use the undefined the oldstate(x) function. As long as we make sure that the oldstate(x) function will be defined and run before the storageflux calculation is run, no exception will be raised.";
+dict[2120] = "Before the loop construct is entered, you can basically copy the previous code that describes the two time increments. Do not forget to assign the proper state values to the oldstate function before entering the while loop.";
+dict[2123] = "Create a function that simulates the potential temperature profile in time taking as its only argument the time step for which should be solved. Return a data container in which the profiles are stored. Run this function in a for loop for every time step and plot the result for comparison.";
 dict[222] = "Think about the different sections in the model setup. First you define your system properties, then the mathematical part and finally the numerical part which makes the model ready to solve.";
 dict[224] = "Several useful data frames are described in the documentation.";
 dict[225] = "In order to add a graph to a figure, first the figure is created with the plot() function, thereafter lines() or points() can be used to add data.";
