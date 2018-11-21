2330de586e82d875a3d099acf7731a34e1d30abe
BramBeer <bram.berendsen@outlook.com>
2018-11-21 14:41:58 +0100

    feedback processed, assignment finalized

--- Assignment4/hints.js
+++ Assignment4/hints.js
@@ -4,7 +4,7 @@ dict[41] = "The domain is defined by coordinates of two dimensions. Therefore, t
 dict[42] = "Remember that a Neumann natural boundary condition is assigned to unspecified boundaries.";
 dict[45] = "Note that you can suppress graphing of the node generation by setting 'verbose=False'.";
 dict[413] = "Change both the domain values of 'xtopl' and 'ytopl' before interpolating the two dimensional model domain of TwoDmodel with the new transmissivities.";
-dict[417] = "add: welldist = func.dist.to.point(c(1000,1000),200,25,100) and set.discretisation(TwoDModelWell, nodes=list(type='geomgen', optdist=welldist, verbose=TRUE), method='FE') on separate lines to decrease the nodal distance around the location of the well.";
+dict[417] = "add: welldist = func.dist.to.point(c(1000,1000),200,25,100) and set.discretisation(TwoDModelWell, nodes=list(type='geomgen', optdist=welldist, verbose=FALSE), method='FE') on separate lines to decrease the nodal distance around the location of the well.";
 dict[419] = "Check the documentation of the func.dist.to.line() function in the FVFE2D package.";
 dict[420] = "Define domain range: plot(c(0,10), c(0,2), xlab='x', ylab='y', col='white') Plot polygon: polygon(domain, lwd=3, col=rgb(0.7,0.7,0.7,0.3)).";
 dict[422] = "Remember the function plot.BC.id().";
