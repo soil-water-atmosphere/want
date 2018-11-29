3cd68faa88cc44f3ff4d1638156695e0f1807fdc
liekemelsen <lieke.melsen@gmail.com>
2018-11-29 15:53:52 -0700

    new assignment for FV

--- newton_raphson/hints.js
+++ newton_raphson/hints.js
@@ -1,7 +1,7 @@
 // The hints are written in a dictionary, the key is the question number
 dict = {};
-dict[1] = "y = function(x){....}";
-dict[2] = "If we approximate non-linear systems with linear ones, we can note that as follows: $f(x) \approx \tilde{f}(x)$";
+dict[1] = "y <- function(x){....}";
+dict[2] = "We approximate non-linear systems with linear ones, nonlin(x)~lin(x), by determining the slope around an estimate of x: lin(x) = lin(estimate)+ ((lin(estimate+epsilon)-lin(estimate))/epsilon)*(x-estimate)";
 dict[3] = "";
 // dict[3] = extend with new hints by extending this dictionary
 
