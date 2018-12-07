f6684d69128f666cdd90c8ca047b5da1bd0fd845
Roebroek <roebroek.jasper@gmail.com>
2018-12-07 10:21:19 +0100

    Updated version of time practical. CvH comments applied

--- finite_difference/Time/hints.js
+++
@@ -1,8 +0,0 @@
-function toggle(ID) {
-    var x = document.getElementById(ID);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
